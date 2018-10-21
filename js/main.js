bacc5df0afd4888d2255d30a7e87a99a60e8c998
Janinasm <janina.martinsen@gmail.com>
2018-10-21 13:45:53 +0200

    removed videofiles etc

--- js/main.js
+++ js/main.js
@@ -21,3 +21,34 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block"; 
 }
+
+// add/remove class on scroll
+$(document).ready(function() {
+	var s = $(".header_landing");
+	var pos = s.position();					   
+	$(window).scroll(function() {
+		var windowpos = $(window).scrollTop();
+		if (windowpos >= pos.top & windowpos <=2000) {
+			s.addClass("stick");
+		} else {
+			s.removeClass("stick");	
+		}
+	});
+});
+
+$(document).ready(function() {
+	var s2 = $(".img-b");
+	var pos2 = s2.position();					   
+	$(window).scroll(function() {
+		var windowpos = $(window).scrollTop();
+		if (windowpos >= pos2.top & windowpos > 2000) {
+            s2.addClass("stick");
+            s2.addClass("visible");
+            s2.addClass("zindex");
+		} else {
+            s2.removeClass("stick");
+            s2.removeClass("visible");
+            s2.removeClass("zindex");
+		}
+	});
+});
