0bbb1372f17a777c3e1062b7eb01e0934261fa98
Janinasm <janina.martinsen@gmail.com>
2018-10-23 23:25:54 +0200

    minor fixes

--- js/main.js
+++ js/main.js
@@ -1,25 +1,26 @@
-
-
-
 // MULTIMEDIA E - IMAGE-BOX
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+  showSlides((slideIndex += n));
+}
 
 function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slides");
+  var i;
+  var slides = document.getElementsByClassName("slides");
 
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
 
-    for(i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block"; 
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
 }
 
 /*
@@ -27,7 +28,6 @@ function showSlides(n) {
 $(window).scroll(function(){
 $(".header_landing_grid").css("opacity", 1 - $(window).scrollTop() / 700);
 });*/
-
 
 /*
 
@@ -58,7 +58,6 @@ window.addEventListener('scroll', function() {
 	console.log(scrollpos)
 })*/
 
-
 var scrollpos = window.scrollY;
 var header = document.getElementById("header_landing");
 var header_height = header.offsetHeight;
@@ -66,8 +65,12 @@ var header_height = header.offsetHeight;
 var header_grid = document.getElementById("header_landing_grid");
 var header_grid_height = header_grid.offsetHeight;
 
+var multimedia_scroll_media = document.getElementById(
+  "multimedia-scroll_media"
+);
 var img1 = document.getElementById("img-1");
 var img2 = document.getElementById("img-2");
+var img3 = document.getElementById("img-3");
 
 var scroll_container = document.getElementById("scroll-helper");
 var scroll_container_height = scroll_container.offsetHeight;
@@ -75,50 +78,83 @@ var scroll_container_height = scroll_container.offsetHeight;
 var multimedia_scroll = document.getElementById("multimedia-scroll");
 var multimedia_scroll_height = multimedia_scroll.offsetHeight;
 
+window.addEventListener("scroll", function() {
+  scrollpos = window.scrollY;
 
-window.addEventListener('scroll', function() {
-	scrollpos = window.scrollY;
+  if (scrollpos > header_height) {
+    header.classList.add("fade-in");
+  } else {
+    header.classList.remove("fade-in");
+  }
 
-	if(scrollpos > header_height) {
-		header.classList.add("fade-in");
-	}
-	else {
-		header.classList.remove("fade-in");
-	}
+  if (scrollpos > 50 && scrollpos <= header_grid_height * 2) {
+    header_grid.classList.add("fade-in");
+  } else {
+    header_grid.classList.remove("fade-in");
+  }
 
-	if(scrollpos > 50 && scrollpos <= header_grid_height * 2) {
-		header_grid.classList.add("fade-in");
-	}
-	else {
-		header_grid.classList.remove("fade-in");
-	}
+  // scroll container
 
-
-
-// scroll container
+  /*
 	if(scrollpos >= scroll_container_height && scrollpos < scroll_container_height + multimedia_scroll_height) {
-		img1.classList.remove("fade-in");
+		mutlimedia_scroll_media.classList.remove("fade-in");
 	}
 	else  {
-		img1.classList.add("fade-in");
+		mutlimedia_scroll_media.classList.add("fade-in");
 	}
 
 
 	if(scrollpos >= scroll_container_height) {
-		img2.classList.add("sticky");
+		img1.classList.add("sticky");
 	}
 	else  {
-		img2.classList.remove("sticky");
+		img1.classList.remove("sticky");
 	}
 
 
 	
 });
+*/
+
+  if (
+    scrollpos >= scroll_container_height &&
+    scrollpos < scroll_container_height + (multimedia_scroll_height )
+  ) {
+    multimedia_scroll_media.classList.remove("fade-in");
+  } else {
+    multimedia_scroll_media.classList.add("fade-in");
+  }
+
+  if (scrollpos >= scroll_container_height) {
+    img1.classList.add("sticky");
+  } else {
+    img1.classList.remove("sticky");
+  }
+
+
+
+
+
+
+
+  if (scrollpos >= scroll_container_height) {
+    img2.classList.add("sticky");
+  } else {
+    img2.classList.remove("sticky");
+  }
+
+
+
+
+  if (scrollpos >= scroll_container_height) {
+	img3.classList.add("sticky");
+  } else {
+    img3.classList.remove("sticky");
+  }
+
+});
 
 /*
-
-
-
 
 
 function addClassOnScroll(elem, class) {
