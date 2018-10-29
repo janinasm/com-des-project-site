df38a04db92e7a7256473e6b1509014109fee1e0
Janinasm <janina.martinsen@gmail.com>
2018-10-29 16:35:02 +0100

    fjerna bilder + scroll indicator + mediaquery + rydda js

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ function plusSlides(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("slides");
+  var numbers = document.getElementsByClassName("number");
 
   if (n > slides.length) {
     slideIndex = 1;
@@ -25,40 +26,6 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
 }
 
-/*
-
-$(window).scroll(function(){
-$(".header_landing_grid").css("opacity", 1 - $(window).scrollTop() / 700);
-});*/
-
-/*
-
-
-let scrollpos = window.scrollY
-const header = document.getElementById("img-2")
-const header_height = header.offsetHeight
-
-const scroll_container = document.getElementsByClassName("multimedia-scroll_media")
-const scroll_container_height = scroll_container.offsetHeight
-
-//const add_class_on_scroll = () => header.classList.add("fade-in");
-//const remove_class_on_scroll = () => header.classList.remove("fade-in")
-
-window.addEventListener('scroll', function() { 
-    scrollpos = window.scrollY;
-    if (scrollpos >= header_height) { 
-		
-		header.classList.add("fade-in");
-		header.classList.remove("sticky");
-		
-	}
-	else { 
-		header.classList.remove("fade-in");
-		header.classList.add("sticky");
-	 }
-	
-	console.log(scrollpos)
-})*/
 
 var scrollpos = window.scrollY;
 var header = document.getElementById("header_landing");
@@ -82,116 +49,10 @@ window.addEventListener("scroll", function() {
     header_grid.classList.remove("fade-out");
   }
 });
-/*
 
-
-var multimedia_scroll_media = document.getElementById(
-  "multimedia-scroll_media"
-);
-var img1 = document.getElementById("img-1");
-var img2 = document.getElementById("img-2");
-var img3 = document.getElementById("img-3");
-
-var scroll_container = document.getElementById("scroll-helper");
-var scroll_container_height = scroll_container.offsetHeight;
-
-var multimedia_scroll = document.getElementById("multimedia-scroll");
-var multimedia_scroll_height = multimedia_scroll.offsetHeight;
-
-
-
- 
-
-  if (
-    scrollpos >= scroll_container_height &&
-    scrollpos < scroll_container_height + (multimedia_scroll_height )
-  ) {
-    multimedia_scroll_media.classList.remove("fade-in");
-  } else {
-    multimedia_scroll_media.classList.add("fade-in");
-  }
-
-
-
-
-
-
-
-  if (scrollpos >= scroll_container_height + (multimedia_scroll_height /2)) {
-    img2.classList.remove("fade-in");
-  } else {
-    img2.classList.add("fade-in");
-  }
-
- // console.log(scrollpos);
-});
-
-
-/*
-
-
-function addClassOnScroll(elem, class) {
-	header.classList.add("fade-in");
-}
-
-function removeClassOnScroll() {
-	header.classList.remove("fade-in");
-}
-
-window.addEventListener('scroll', function() {
-	scrollpos = window.scrollY;
-
-
-	if(scrollpos > header_height) {
-		header.classList.add("fade-in");
-	}
-	else {
-		header.classList.remove("fade-in");
-	}
-
-
-	if(scrollpos > 0 && scrollpos <= header_grid_height * 2) {
-		header_grid.classList.add("fade-in");
-	}
-	else {
-		header_grid.classList.remove("fade-in");
-	}
-
-
-*/
- // scroll container
-
-  /*
-	if(scrollpos >= scroll_container_height && scrollpos < scroll_container_height + multimedia_scroll_height) {
-		mutlimedia_scroll_media.classList.remove("fade-in");
-	}
-	else  {
-		mutlimedia_scroll_media.classList.add("fade-in");
-	}
-
-
-	if(scrollpos >= scroll_container_height) {
-		img1.classList.add("sticky");
-	}
-	else  {
-		img1.classList.remove("sticky");
-	}
-
-
-	
-});
-*/
-
-/*
-function isInViewport(elem) {
-  let elemTop = elem.offsetTop;
-  let elemBottom = elemTop + elem.height;
-  let viewportTop = window.scrollY;
-  let viewportBottom = viewportTop + window.innerHeight;
-  return elemBottom > viewportTop && elemTop < viewportBottom;
-};*/
 
 var multimedia_scroll = document.getElementById('multimedia-scroll');
+var multimedia_scroll_media = document.getElementById('multimedia-scroll_media');
 
 var quote1 = document.getElementsByClassName('multimedia-scroll_text_quoteblock')[1];
 var quote2 = document.getElementsByClassName('multimedia-scroll_text_quoteblock')[2];
@@ -206,18 +67,18 @@ var visible = false;
 // https://jsfiddle.net/shaaraddalvi/4rp09jL0/
 window.addEventListener('scroll', function(e) {
 
-
   if(isScrolledIntoView(multimedia_scroll)) {
+
+    multimedia_scroll_media.classList.remove('not-visible');
+
     img1.classList.remove('fade-out');
+
   } 
 
   if(!isScrolledIntoView(multimedia_scroll)) {
-    img1.classList.add('fade-out');
-    img2.classList.add('fade-out');
-    img3.classList.add('fade-out');
+    multimedia_scroll_media.classList.add('not-visible');
+
   } 
-
-
 
 	if(isScrolledIntoView(quote1)) {
     img2.classList.add('fade-out');
